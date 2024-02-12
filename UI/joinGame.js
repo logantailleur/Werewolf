7c23a5b5a0a7db4b3ff6bcd8e7e11a9acf9a1f17
Ethan L. Kerr <ethan.kerr@macbook-pro-224.mynetworksettings.com>
2024-02-12 16:59:28 -0700

    Add name entry and join game conditions.

--- UI/joinGame.js
+++ UI/joinGame.js
@@ -1,9 +1,10 @@
 function checkInput() {
-    var input = document.getElementById("gameCodeInput");
+    var gameCodeInput = document.getElementById("gameCodeInput");
+    var nameInput = document.getElementById("nameInput")
     var button = document.getElementById("joinGameButton");
     
     // Enable the button if the input length is 5, disable it otherwise
-    if (input.value.length === 5) {
+    if (gameCodeInput.value.length === 5 && nameInput.value.length >= 1) {
         button.disabled = false;
         button.classList.remove("disabled-button"); // Remove the disabled styling
     } else {
