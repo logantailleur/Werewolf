2ac5e66a69311b46280effe0b475f9b40f217176
Ethan L. Kerr <ethan.kerr@MacBook-Pro-224.app.byu.edu>
2024-02-14 15:19:09 -0700

    Adjust role image and make text dynamic based on js.

--- UI/roleAssign.js
+++ UI/roleAssign.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get a reference to the View Role button
+    // Get references to document portions
     var viewRoleButton = document.getElementById("viewRoleBtn");
+    var roleAnnouncement = document.getElementById("roleAnnouncement");
+    var roleSummary = document.getElementById("roleSummary");
+
+    // Change the role announcement
+    roleAnnouncement.textContent = "You are a [role]!"
+
+    // Change the role summary
+    roleSummary.textContent = "Summary of role"
 
     // Add click event listener to the View Role button
     viewRoleButton.addEventListener("click", function() {
 
         // Get a reference to the image element
         var roleImage = document.querySelector(".role-assign-image img");
-
+        
         // Set the src attribute to the desired image path
         roleImage.src = "ImageAssets/wolficon.png";
 
@@ -15,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var viewRoleTitle = document.getElementById("viewRoleTitle");
         var viewRoleObjective = document.getElementById("viewRoleObjective");
         var viewRoleAbilities = document.getElementById("viewRoleAbilities");
+
         // Change the modal title
         viewRoleTitle.textContent = "Role Title";
 
