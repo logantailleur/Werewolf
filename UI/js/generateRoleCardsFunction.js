554277cdd11dd82fbe68bb2569e0e85c26bbb9dc
Ethan L. Kerr <ethan.kerr@macbook-pro-224.mynetworksettings.com>
2024-03-08 15:26:38 -0700

    Add random dead cards.

--- UI/js/generateRoleCardsFunction.js
+++ UI/js/generateRoleCardsFunction.js
@@ -1,6 +1,5 @@
 import { roleCardHTML } from "./roleCard.js";
 
-//Generate initial role cards (e.g., 10).
 //TODO: Adjust the number of roles as needed.
 export function generateRoleCards() {
     //Clear previous role cards.
@@ -14,6 +13,15 @@ export function generateRoleCards() {
         let roleCardElement = document.createElement("div");
         //Adjust width and margin as needed.
         roleCardElement.classList.add("col-lg-3", "col-md-4", "col-sm-6", "mb-3", "role-card");
+
+        // Determine if the player is dead or alive
+        const isPlayerDead = isDead();
+
+        // If dead, make the card non-selectable
+        if (isPlayerDead){
+            roleCardElement.classList.add("dead");
+        }
+
         roleCardElement.innerHTML = roleCardHTML;
 
         // Generate ID for the card. 
@@ -27,4 +35,10 @@ export function generateRoleCards() {
         //Append role card to container.
         document.getElementById("otherRoleCards").appendChild(roleCardElement);
     }
+}
+
+function isDead() {
+    // TODO: Do this based on their status in the database
+    const randNum = Math.random();
+    return randNum < 0.5;
 }
