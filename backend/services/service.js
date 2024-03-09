97d056a843c0b2168eddfc2285b0d6ebd729b4e3
logantailleur <logantailleur@gmail.com>
2024-03-09 13:39:08 -0700

    Add HTTP methods

--- backend/services/service.js
+++ backend/services/service.js
@@ -57,4 +57,146 @@ async function viewRole(gameCode, playerId) {
 	}
 }
 
-module.exports = { createGame, beginGame, joinRunningGame, viewRole };
+async function getAllRoles(gameCode) {
+	return {
+		success: true,
+		players: [
+			{
+				name: 'Player 1',
+				playerId: 1,
+				role: 'werewolf',
+			},
+			{
+				name: 'Player 2',
+				playerId: 2,
+				role: 'villager',
+			},
+			{
+				name: 'Player 3',
+				playerId: 3,
+				role: 'villager',
+			},
+			{
+				name: 'Player 4',
+				playerId: 4,
+				role: 'villager',
+			},
+			{
+				name: 'Player 5',
+				playerId: 5,
+				role: 'villager',
+			},
+			{
+				name: 'Player 6',
+				playerId: 6,
+				role: 'villager',
+			},
+		],
+	};
+}
+
+async function hostSleeps(gameCode) {
+	//Change gameState to 4
+	return {
+		success: true,
+		canContinue: true,
+	};
+}
+
+async function hostWakes(gameCode) {
+	//Change gameState to 6
+	return {
+		success: true,
+		canContinue: true,
+	};
+}
+
+async function endVoting(gameCode) {
+	//Change gameState to 9
+	return {
+		success: true,
+		canContinue: true,
+	};
+}
+
+async function playerSleeps(gameCode, playerId) {
+	//Sets player sleeping to true. If all players are sleeping, Change gameState to 3
+	//Return all alive players to possibly display to werewolf role
+	return {
+		success: true,
+		players: [
+			{
+				name: 'Player 2',
+				playerId: 2,
+				role: 'villager',
+			},
+			{
+				name: 'Player 3',
+				playerId: 3,
+				role: 'villager',
+			},
+			{
+				name: 'Player 4',
+				playerId: 4,
+				role: 'villager',
+			},
+			{
+				name: 'Player 5',
+				playerId: 5,
+				role: 'villager',
+			},
+			{
+				name: 'Player 6',
+				playerId: 6,
+				role: 'villager',
+			},
+		],
+	};
+}
+
+async function playerWakes(gameCode, playerId) {
+	//Sets player sleeping to false. If all players are awake, Change gameState to 5
+	return {
+		success: true,
+		canContinue: true,
+	};
+}
+
+async function werewolfKills(gameCode, playerId, victimPlayerId) {
+	//Sets player sleeping to false, sets victimPlayerId to dead. If all players are awake, Change gameState to 5
+	return {
+		success: true,
+		canContinue: true,
+	};
+}
+
+async function playerReadyToVote(gameCode, playerId) {
+	//Sets player readyToVote to true. If all players are readyToVote
+	return {
+		success: true,
+		canContinue: true,
+	}
+}
+
+async function playerVote(gameCode, playerId, voteId) {
+	//Sets player readyToVote to false. If all players are !readyToVote, count votes and change gameState to 10
+	return {
+		success: true,
+	}
+}
+
+module.exports = {
+	createGame,
+	beginGame,
+	joinRunningGame,
+	viewRole,
+	getAllRoles,
+	hostSleeps,
+	hostWakes,
+	endVoting,
+	playerSleeps,
+	werewolfKills,
+	playerReadyToVote,
+	playerVote,
+	playerWakes,
+};
