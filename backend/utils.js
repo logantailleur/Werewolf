5d41148f0a52924a86ade94158334a2782c7c187
Ethan L. Kerr <ethan.kerr@MacBook-Pro-224.app.byu.edu>
2024-03-18 13:20:53 -0600

    Modularize role card click within js files.

--- backend/utils.js
+++
@@ -1,6 +0,0 @@
-function generateRandom5DigitNumber() {
-    const randomNumber = Math.floor(Math.random() * 90000) + 10000;
-    return randomNumber;
-}
-
-module.exports = { generateRandom5DigitNumber };
